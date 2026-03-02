5c791f47e649cd06deb94e0b3796214153af8c3a
ipykcan <poojasreebhogavalli@gmail.com>
2026-03-02 08:48:52 +0530

    metrics

--- api/weather.js
+++ api/weather.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const city = req.query.city;
 
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
 
   const data = await response.json();
